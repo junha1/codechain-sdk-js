68012341618cb07483d20bccedd76434428fa94b
Junha Yang0 <junha@codechain.io>
2020-02-10 14:33:51 +0900

    QweqweqweqweqwE

--- lib/core/transaction/json.d.ts
+++
@@ -1,9 +0,0 @@
-import { SignedTransaction, Transaction } from "../classes";
-import { SignedTransactionJSON } from "../SignedTransaction";
-export declare function fromJSONToTransaction(result: any): Transaction;
-/**
- * Create a SignedTransaction from a SignedTransaction JSON object.
- * @param data A SignedTransaction JSON object.
- * @returns A SignedTransaction.
- */
-export declare function fromJSONToSignedTransaction(data: SignedTransactionJSON): SignedTransaction;
